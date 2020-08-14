86003e6e2728bea5e211f6bd13a69348f8467241
bernhard kissler <bernhard.kissler@outlook.de>
2020-08-14 14:53:18 +0200

    added data table input and improve structure

--- base/app/app.py
+++ base/app/app.py
@@ -3,7 +3,8 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import dash_table
 
 import main_functions as mf
 
@@ -11,24 +12,87 @@ app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(
     children=[
-        dbc.Col(
-            [
-                html.H1(children="Hello Dash"),
-                dbc.Container(
-                    dbc.Alert("Hello Bootstrap!", color="success"), className="p-5",
+        dbc.Navbar(
+            dbc.Row(
+                [
+                    dbc.NavbarBrand(
+                        "Risky decisions - Tool", className="ml-5 text-white"
+                    ),
+                    # dbc.NavItem(dbc.NavLink("Top", href="#")),
+                ]
+            ),
+            # brand="Risky Decisions - Tool",
+            # brand_href="#",
+            color="dark",
+        ),
+        dbc.Row(
+            children=[
+                dbc.Col(
+                    [
+                        dbc.Container(
+                            [
+                                html.H3(children="Inputs", className="py-2"),
+                                dbc.Label("Payoffs"),
+                                dbc.Input(
+                                    id="pays",
+                                    type="text",
+                                    placeholder="list of payoffs",
+                                    debounce=True,
+                                ),
+                                dbc.Label("Probabilities"),
+                                dbc.Input(
+                                    id="probs",
+                                    type="text",
+                                    placeholder="list of probabilities",
+                                    debounce=True,
+                                ),
+                                html.Hr(className=""),
+                                dash_table.DataTable(
+                                    id="input_tbl",
+                                    columns=(
+                                        [
+                                            {
+                                                "id": "payoffs_tbl",
+                                                "name": "Payoffs",
+                                                "type": "numeric",
+                                            }
+                                        ]
+                                        + [
+                                            {
+                                                "id": "probabilities_tbl",
+                                                "name": "Probabilities",
+                                                "type": "numeric",
+                                            }
+                                        ]
+                                    ),
+                                    data=[dict(payoffs_tbl=1, probabilities_tbl=1)],
+                                    editable=True,
+                                    row_deletable=True,
+                                ),
+                                dbc.Button(
+                                    "Add Row",
+                                    id="editing-rows-button",
+                                    n_clicks=0,
+                                    className="my-2",
+                                ),
+                            ],
+                            className="px-1 mx-2",
+                        ),
+                        dbc.Container(
+                            [
+                                html.H3(children="Output", className="py-2"),
+                                html.Div(id="output"),
+                                html.Hr(className=""),
+                                html.Div(id="output_new"),
+                            ],
+                            className="px-1 mx-2",
+                        ),
+                    ],
+                    width=10,
                 ),
-                dbc.Input(
-                    id="pays", type="text", placeholder="list of payoffs", debounce=True
-                ),
-                dbc.Input(
-                    id="probs",
-                    type="text",
-                    placeholder="list of probabilities",
-                    debounce=True,
-                ),
-                html.Div(id="output"),
-            ]
-        )
+            ],
+            justify="center",
+        ),
     ]
 )
 
@@ -45,6 +109,33 @@ def update_output(input1, input2):
 
     return "Payoffs {} and Probabilities {} for an expected utility of {} and a CPT value of {}".format(
         input1_pr, input2_pr, exp_util, cum_pros_theor
+    )
+
+
+@app.callback(
+    Output("input_tbl", "data"),
+    [Input("editing-rows-button", "n_clicks")],
+    [State("input_tbl", "data"), State("input_tbl", "columns")],
+)
+def add_row(n_clicks, rows, columns):
+    if n_clicks > 0:
+        rows.append({c["id"]: "" for c in columns})
+    return rows
+
+
+@app.callback(
+    Output("output_new", "children"),
+    [Input("input_tbl", "data"), Input("input_tbl", "columns")],
+)
+def update_output_new(rows, columns):
+    pays = [i["payoffs_tbl"] for i in rows]
+    probs = [i["probabilities_tbl"] for i in rows]
+    print(pays, probs)
+    exp_util = mf.expected_utility(pays, probs)
+    cum_pros_theor = mf.cumulative_prospect_theory(pays, probs)
+
+    return "Payoffs {} and Probabilities {} for an expected utility of {} and a CPT value of {}".format(
+        pays, probs, exp_util, cum_pros_theor
     )
 
 
