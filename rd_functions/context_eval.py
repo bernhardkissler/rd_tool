30b11ba2bcadc034642507de3f48399a92a46e13
bernhard kissler <bernhard.kissler@outlook.de>
2020-11-14 00:13:14 +0100

    added user_regret function and included it with simpleeval by using functools eval

--- rd_functions/context_eval.py
+++ rd_functions/context_eval.py
@@ -1,3 +1,9 @@
+import rd_functions.util_mod as um
+from simpleeval import simple_eval
+import math
+from functools import partial
+
+
 def ls_regret(
     x_1, x_2, weight, um_function, um_kwargs={},
 ):
@@ -5,3 +11,31 @@ def ls_regret(
     return um_function(x_1, **um_kwargs) + weight * (
         um_function(x_1, **um_kwargs) - um_function(x_2, **um_kwargs)
     )
+
+
+def user_regret(x: float, text: str, um_function=um.lin_utility, um_kwargs={}) -> float:
+    """ 
+    Takes in a string and evaluates it (safely) with the simpleeval
+    model to allow users to define their own regret functions
+    """
+    util_func = partial(um_function, **um_kwargs)
+
+    res = simple_eval(
+        text,
+        functions={
+            # "print": print,
+            "abs": abs,
+            "sin": math.sin,
+            "cos": math.cos,
+            "tan": math.tan,
+            "e": math.e,
+            "exp": math.exp,
+            "log": math.log,
+            "log10": math.log10,
+            "pi": math.pi,
+            "sqrt": math.sqrt,
+            "u": util_func,
+        },
+        names={"x": x},
+    )
+    return res
