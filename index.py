5501b8605cedef38d62840631b55e27c60b7f85b
bernhard kissler <bernhard.kissler@outlook.de>
2020-11-21 01:28:38 +0100

    Finally deleted navbar

--- index.py
+++ index.py
@@ -11,62 +11,22 @@ from apps import input_rd, main_rd, output_rd
 sub_bg_color = "rgba(255,255,255,1)"
 prim_color = "#e3685f"
 
-navbar = dbc.Navbar(
-    [
-        dbc.NavbarBrand(
-            html.A("Choices under risk - tool", href="#", className="text-white"),
-        ),
-        dbc.Collapse(
-            [
-                # FIXME Navbar verdeckt teilweise content, kann der Link offset geändert werden?
-                # dbc.NavItem(
-                #     html.A("Utility Link", href="#uf_link", className="text-white"),
-                # ),
-                # dbc.NavItem(
-                #     html.A("Probability Link", href="#pw_link", className="text-white"),
-                # ),
-                # dbc.NavItem(
-                #     html.A("Output Link", href="#output_link", className="text-white"),
-                # ),
-            ],
-            id="navbar-collapse",
-            navbar=True,
-        ),
-        dbc.NavbarToggler(id="navbar-toggler"),
-    ],
-    color="dark",
-    fixed="top",
-)
-# add callback for toggling the collapse on small screens
-# @app.callback(
-#     Output("navbar-collapse", "is_open"),
-#     [Input("navbar-toggler", "n_clicks")],
-#     [State("navbar-collapse", "is_open")],
-# )
-# def toggle_navbar_collapse(n, is_open):
-#     if n:
-#         return not is_open
-#     return is_open
-
 
 app.layout = html.Div(
     [
-        # html.Div(html.Div(navbar, className="col-12"), className="row",),
         html.Div(
             [
                 html.Div(className="col-2 d-sm-none d-md-block d-print-none"),
                 html.Div(
                     html.Div(
                         [
-                            main_rd.theor_segment,
+                            html.H1("Decisions under Uncertainty - Tool"),
                             input_rd.input_segment,
-                            html.Hr(),
                             main_rd.um_segment,
                             main_rd.pw_segment,
                             main_rd.rg_segment,
                             main_rd.sl_segment,
                             main_rd.sdt_segment,
-                            html.Hr(),
                             output_rd.output_segment,
                             output_rd.ce_toast,
                             main_rd.toast_1,
